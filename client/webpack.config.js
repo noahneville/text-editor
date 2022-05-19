d2c0cffb8c22b979fe38a089431abbc2252b53b0
Noah Brunner <noahneville@gmail.com>
2022-05-18 21:31:15 -0400

    added plugins and rules to webpack config file

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -18,12 +18,61 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      // plugins allow us to add additional features and functionalities when we bundle our code. 
+      // Html Webpack creates our html off of the template given, index.html
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'Just Another Text Editor'
+      }),
+     
+      // injects our service worker file
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: 'src-sw.js',
+      }),
+
+      // Creates the manifest file
+      new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
+        name: 'Just Another Text Editor',
+        short_name: 'J.A.T.E',
+        description: 'A lightweight text editor that you can use in browser or offline as a PWA',
+        background_color: '#225ca3',
+        theme_color: '#225ca3',
+        start_url: './',
+        publicPath: './',
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+          },
+        ],
+      }),
     ],
 
     module: {
+      // rules are used to group things that you want bundled together in your build. 
+      // the first rule, 'test: /\.css$/' is a regex grabbing all files ending in .css
+      // the use: styleloader, css-loader tells webpack what we want to use to bundle these files. 
       rules: [
-        
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          // babel is a transpiler (transforms and compiles all at once) We can configure that using the options. 
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+            },
+          },
+        },
       ],
     },
   };
